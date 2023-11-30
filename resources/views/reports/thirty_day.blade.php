1802ab48ce88227d09524bd33c6508bb4727306f
ralph Samonte pabua <87641822+pabuaralph55@users.noreply.github.com>
2023-12-01 06:06:02 +0800

    Update

--- resources/views/reports/thirty_day.blade.php
+++ resources/views/reports/thirty_day.blade.php
@@ -3,83 +3,83 @@
 @section('title', 'Data Analysis')
 
 @section('content')
-<h1>30-day Report</h1>
-<form action="#" method="GET" class="row g-3 align-items-end">
-    <div class="col-auto">
-        <label for="start_date" class="form-label mb-0">Start Date</label>
-        <input type="date" id="start_date" name="start_date" class="form-control" 
-                value="{{ request('start_date') }}">
-    </div>
-    <div class="col-auto">
-        <label for="end_date" class="form-label mb-0">End Date</label>
-        <input type="date" id="end_date" name="end_date" class="form-control" 
-                value="{{ request('end_date') }}">
-    </div>
-    <div class="col-auto">
-        <button id="retrieve_data" type="submit" class="btn btn-primary">Retrieve Data</button>
-    </div>
-</form>
+    <h1>30-day Report</h1>
+    <form action="#" method="GET" class="row g-3 align-items-end">
+        <div class="col-auto">
+            <label for="start_date" class="form-label mb-0">Start Date</label>
+            <input type="date" id="start_date" name="start_date" class="form-control" 
+                    value="{{ request('start_date') }}">
+        </div>
+        <div class="col-auto">
+            <label for="end_date" class="form-label mb-0">End Date</label>
+            <input type="date" id="end_date" name="end_date" class="form-control" 
+                    value="{{ request('end_date') }}">
+        </div>
+        <div class="col-auto">
+            <button id="retrieve_data" type="submit" class="btn btn-primary">Retrieve Data</button>
+        </div>
+    </form>
 
-@if ($showingDefaultData)
-    <div class="alert alert-info">
-        No data found for the selected date range. Showing the latest 30 days of data instead.
-    </div>
-@endif
+    @if ($showingDefaultData)
+        <div class="alert alert-info">
+            No data found for the selected date range. Showing the latest 30 days of data instead.
+        </div>
+    @endif
 
-<table>
-    <thead>
-        <tr>
-            <th>#</th> 
-            <th>
-                <a href="{{ route('reports.thirty_day', ['start_date' => request('start_date'), 
-                    'end_date' => request('end_date'),'sortColumn' => 'day', 'sortOrder' => request('sortOrder', 'asc') == 'asc' ? 'desc' : 'asc']) }}">
-                    Day
-                    @if (request('sortColumn') == 'day')
-                        {!! request('sortOrder', 'asc') == 'asc' ? '&#x25BC;' : '&#x25B2;' !!}
-                    @endif
-                </a>
-            </th>
-            <th>
-                <a href="{{ route('reports.thirty_day', ['start_date' => request('start_date'), 
-                    'end_date' => request('end_date'), 'sortColumn' => 'total_impressions', 'sortOrder' => request('sortOrder', 'asc') == 'asc' ? 'desc' : 'asc']) }}">
-                    Impressions
-                    @if (request('sortColumn') == 'total_impressions')
-                        {!! request('sortOrder', 'asc') == 'asc' ? '&#x25BC;' : '&#x25B2;' !!}
-                    @endif
-                </a>
-            </th>
-            <th>
-                <a href="{{ route('reports.thirty_day', ['start_date' => request('start_date'), 
-                    'end_date' => request('end_date'), 'sortColumn' => 'total_conversions', 'sortOrder' => request('sortOrder', 'asc') == 'asc' ? 'desc' : 'asc']) }}">
-                    Conversions
-                    @if (request('sortColumn') == 'total_conversions')
-                        {!! request('sortOrder', 'asc') == 'asc' ? '&#x25BC;' : '&#x25B2;' !!}
-                    @endif
-                </a>
-            </th>
-            <th>
-                <a href="{{ route('reports.thirty_day', ['start_date' => request('start_date'), 
-                    'end_date' => request('end_date'), 'sortColumn' => 'conversion_rate', 'sortOrder' => request('sortOrder', 'asc') == 'asc' ? 'desc' : 'asc']) }}">
-                    Conversion Rate
-                    @if (request('sortColumn') == 'conversion_rate')
-                        {!! request('sortOrder', 'asc') == 'asc' ? '&#x25BC;' : '&#x25B2;' !!}
-                    @endif
-                </a>
-            </th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($stats as $index => $stat)
+    <table>
+        <thead>
             <tr>
-                <td>{{ $index + 1 }}</td>
-                <td>{{ $stat->day }}</td>
-                <td>{{ number_format($stat->total_impressions, 0, '.', ',') }}</td>
-                <td>{{ number_format($stat->total_conversions, 0, '.', ',') }}</td>
-                <td>{{ number_format($stat->conversion_rate, 4) }}%</td>
+                <th>#</th> 
+                <th>
+                    <a href="{{ route('reports.thirty_day', ['start_date' => request('start_date'), 
+                        'end_date' => request('end_date'),'sortColumn' => 'day', 'sortOrder' => request('sortOrder', 'asc') == 'asc' ? 'desc' : 'asc']) }}">
+                        Day
+                        @if (request('sortColumn') == 'day')
+                            {!! request('sortOrder', 'asc') == 'asc' ? '&#x25BC;' : '&#x25B2;' !!}
+                        @endif
+                    </a>
+                </th>
+                <th>
+                    <a href="{{ route('reports.thirty_day', ['start_date' => request('start_date'), 
+                        'end_date' => request('end_date'), 'sortColumn' => 'total_impressions', 'sortOrder' => request('sortOrder', 'asc') == 'asc' ? 'desc' : 'asc']) }}">
+                        Impressions
+                        @if (request('sortColumn') == 'total_impressions')
+                            {!! request('sortOrder', 'asc') == 'asc' ? '&#x25BC;' : '&#x25B2;' !!}
+                        @endif
+                    </a>
+                </th>
+                <th>
+                    <a href="{{ route('reports.thirty_day', ['start_date' => request('start_date'), 
+                        'end_date' => request('end_date'), 'sortColumn' => 'total_conversions', 'sortOrder' => request('sortOrder', 'asc') == 'asc' ? 'desc' : 'asc']) }}">
+                        Conversions
+                        @if (request('sortColumn') == 'total_conversions')
+                            {!! request('sortOrder', 'asc') == 'asc' ? '&#x25BC;' : '&#x25B2;' !!}
+                        @endif
+                    </a>
+                </th>
+                <th>
+                    <a href="{{ route('reports.thirty_day', ['start_date' => request('start_date'), 
+                        'end_date' => request('end_date'), 'sortColumn' => 'conversion_rate', 'sortOrder' => request('sortOrder', 'asc') == 'asc' ? 'desc' : 'asc']) }}">
+                        Conversion Rate
+                        @if (request('sortColumn') == 'conversion_rate')
+                            {!! request('sortOrder', 'asc') == 'asc' ? '&#x25BC;' : '&#x25B2;' !!}
+                        @endif
+                    </a>
+                </th>
             </tr>
-        @endforeach
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+            @foreach($stats as $index => $stat)
+                <tr>
+                    <td>{{ $index + 1 }}</td>
+                    <td>{{ $stat->day }}</td>
+                    <td>{{ number_format($stat->total_impressions, 0, '.', ',') }}</td>
+                    <td>{{ number_format($stat->total_conversions, 0, '.', ',') }}</td>
+                    <td>{{ number_format($stat->conversion_rate, 4) }}%</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 
 @endsection
 
